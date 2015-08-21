d9046e3b6545a1a1af45bd511be8684c1f94350a
Daizo SASAKI <d_3@m.ieice.org>
2015-08-21 12:50:39 +0900

    reducing velocity

--- UnityKinect/Assets/Plugins/MyTraceObj.cs
+++ UnityKinect/Assets/Plugins/MyTraceObj.cs
@@ -7,8 +7,11 @@ public class MyTraceObj : MonoBehaviour {
 	public GameObject[] ObjTaget;
 	private int NumObjTgt;
 	private bool FlagActive;
-	public float fVectCam = 0.01f;
-	public double CamDistance; 
+	public float fVelCamCurr = 0.01f;
+	private float fDefaultVelCam = 0.01f;
+	public double CamDistanceToObj; 
+	private double StopDistanceToObj=2.0f;
+	private double dFactorReduce = 5f;
 
 
 	// Use this for initialization
@@ -29,23 +32,26 @@ public class MyTraceObj : MonoBehaviour {
 		if (FlagActive == false) {
 						return;
 				}
-		CamDistance = Vector3.Distance (this.transform.position, ObjTaget [IndxTraceObj].transform.position);
-		if ( CamDistance <= 1) {
+		CamDistanceToObj = Vector3.Distance (this.transform.position, ObjTaget [IndxTraceObj].transform.position);
+		if ( CamDistanceToObj <= StopDistanceToObj) {
 			IndxTraceObj ++;
+			fVelCamCurr = fDefaultVelCam;
 		}
 		if (IndxTraceObj >= (NumObjTgt)) {
 			FlagActive = false;
 			return;
 				}
 		this.transform.LookAt (ObjTaget [IndxTraceObj].transform.position);
+		CamDistanceToObj = Vector3.Distance (this.transform.position, ObjTaget [IndxTraceObj].transform.position);
+		fVelCamCurr = fDefaultVelCam * (float)(1 - System.Math.Exp (-CamDistanceToObj/dFactorReduce));
 
 		Vector3 vectDir = new Vector3 (0, 0, 0);
 		//vectDir = Vector3.MoveTowards (ObjTaget [IndxTraceObj - 1].transform.position, ObjTaget [IndxTraceObj].transform.position, fVectCam);
 		//vectDir = Vector3.MoveTowards (this.transform.position, ObjTaget [IndxTraceObj].transform.position, fVectCam);
 		//vectDir = Vector3.MoveTowards (ObjTaget [IndxTraceObj].transform.position, this.transform.position, fVectCam);
-		vectDir[0] = (ObjTaget [IndxTraceObj].transform.position[0] - this.transform.position[0])*fVectCam;
-		vectDir[1] = (ObjTaget [IndxTraceObj].transform.position[1] - this.transform.position[1])*fVectCam;
-		vectDir[2] = (ObjTaget [IndxTraceObj].transform.position[2] - this.transform.position[2])*fVectCam;
+		vectDir[0] = (ObjTaget [IndxTraceObj].transform.position[0] - this.transform.position[0])*fVelCamCurr;
+		vectDir[1] = (ObjTaget [IndxTraceObj].transform.position[1] - this.transform.position[1])*fVelCamCurr;
+		vectDir[2] = (ObjTaget [IndxTraceObj].transform.position[2] - this.transform.position[2])*fVelCamCurr;
 		this.transform.Translate (vectDir);
 	
 	}
