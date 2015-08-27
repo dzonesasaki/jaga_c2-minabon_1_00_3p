2aa0408adef16ce23f7c9cda14b20e2e24d61536
Daizo SASAKI <d_3@m.ieice.org>
2015-08-27 12:43:37 +0900

    add for a big OSHISHO-san

---
+++ UnityKinect/Assets/Plugins/FreeWalker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreeWalker : MonoBehaviour {
+
+	double x;
+	double y;
+	float speed = 5;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(0, 0.3f, 1);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate(0, -0.3f, -1);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(1, 0, 0);
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(-1, 0, 0);
+		}
+
+		if(Input.GetMouseButton(0)){
+			x -= Input.GetAxis("Mouse X") *speed*2;
+			y += Input.GetAxis("Mouse Y") *speed;
+			transform.rotation = Quaternion.Euler((float)y,(float)x,0);
+		}
+	}
+}
