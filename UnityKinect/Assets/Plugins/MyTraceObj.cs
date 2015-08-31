396424405ae80c99911d12139133657b305f4a62
Daizo SASAKI <d_3@m.ieice.org>
2015-08-31 12:53:22 +0900

    modified Screen Recoding

--- UnityKinect/Assets/Plugins/MyTraceObj.cs
+++ UnityKinect/Assets/Plugins/MyTraceObj.cs
@@ -11,9 +11,16 @@ public class MyTraceObj : MonoBehaviour {
 	private float fDefaultVelCam = 0.03f;
 	public double CamDistanceToObj; 
 	private double StopDistanceToObj=2.0f;
-	private double StopDistanceMargin=0.2f;
+	private double StopDistanceMargin=0.3f;
 	private double dFactorReduce = 1f;
 
+	// screenRecoding , refere to https://gist.github.com/keijiro/7429201
+	public int framerate = 30;
+	public int superSize;
+	public bool autoRecord;
+
+	int frameCount;
+	bool recording;
 
 	// Use this for initialization
 	void Start () {
@@ -25,9 +32,20 @@ public class MyTraceObj : MonoBehaviour {
 			FlagActive = true;
 			IndxTraceObj =1;
 		}
+		StartRecording ();
 	
 	}
-	
+
+	// screenRecoding , refere to https://gist.github.com/keijiro/7429201
+	void StartRecording ()
+	{
+		System.IO.Directory.CreateDirectory ("Capture");
+		Time.captureFramerate = framerate;
+		frameCount = -1;
+		recording = true;
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 		if (FlagActive == false) {
@@ -40,6 +58,7 @@ public class MyTraceObj : MonoBehaviour {
 		}
 		if (IndxTraceObj >= (NumObjTgt)) {
 			FlagActive = false;
+			recording = false;
 			return;
 				}
 		this.transform.LookAt (ObjTaget [IndxTraceObj].transform.position);
@@ -54,6 +73,23 @@ public class MyTraceObj : MonoBehaviour {
 		vectDir[1] = (ObjTaget [IndxTraceObj].transform.position[1] - this.transform.position[1])*fVelCamCurr;
 		vectDir[2] = (ObjTaget [IndxTraceObj].transform.position[2] - this.transform.position[2])*fVelCamCurr;
 		this.transform.Translate (vectDir);
+
+		// screenRecoding , refere to https://gist.github.com/keijiro/7429201
+		if (recording)
+		{
+				if (frameCount > 0)
+				{
+					var name = "Capture/frame" + frameCount.ToString ("0000") + ".png";
+					Application.CaptureScreenshot (name, superSize);
+				}
+				
+				frameCount++;
+				
+				if (frameCount > 0 && frameCount % 60 == 0)
+				{
+					Debug.Log ((frameCount / 60).ToString() + " seconds elapsed.");
+				}
+		}
 	
 	}
 }
